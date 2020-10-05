1a490ff3c885876bbe4cfed790ef1e7dc022f11c
Ryan Oliveira013 <RyanOliveira.2003.c@gmail.com>
2020-10-04 22:48:58 -0300

    Calcule a área de um triângulo retângulo, dados base (b) e altura (h). A área é dada pela metade do produto da base pela altura.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AreaTrianguloRet_At_Etec_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double PrimeiroNumero;
+            double SegundoNumero;
+            
+            Console.Write("Base: ");
+            PrimeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Altura: ");
+            SegundoNumero = Convert.ToDouble(Console.ReadLine());
+            
+            double resultado = PrimeiroNumero / SegundoNumero;
+
+            Console.WriteLine($"A area e: {resultado:N} ");
+        }
+    }
+}
